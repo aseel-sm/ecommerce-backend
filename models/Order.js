ea7316120c387b95a85c8a80a36caf1f21a0e773
aseel-sm <aseelsm0027@gmail.com>
2022-01-21 20:42:14 +0530

    added order api

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema(
+const OrderSchema = new mongoose.Schema(
   {
     userId: {
       type: String,
@@ -29,4 +29,4 @@ const CartSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("Cart", CartSchema);
+module.exports = mongoose.model("Order", OrderSchema);
--- routes/order.js
+++ routes/order.js
@@ -1,7 +1,105 @@
 const router = require("express").Router();
 
-router.get("/usertest", (req, res) => {
-  res.send("success");
+const Order = require("../models/Order");
+const {
+  verifyTokenAndAdmin,
+  verifyTokenAndAuthorization,
+  verifyToken,
+} = require("./verifyToken");
+// create
+
+router.post("/create", verifyTokenAndAuthorization, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// update
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(201).json(updatedOrder);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// delete order
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+
+    res.status(200).json("Order deleted");
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// get user order
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId });
+
+    res.status(200).json(orders);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// get all orders
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const orders = await Order.find();
+
+    res.status(200).json(orders);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// get income
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const prevMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: {
+          createdAt: {
+            $gte: prevMonth,
+          },
+        },
+      },
+      {
+        $project: {
+          month: {
+            $month: "$createdAt",
+          },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
+    ]);
+    res.status(200).json(income);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 });
 
 module.exports = router;
